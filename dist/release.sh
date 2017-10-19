730e65ebce45c53c2e2d7b25b20f52ffecad045c
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-19 01:50:52 +0000

    add installers and repo builds
    Former-commit-id: 7db6d42294149b1d8938dd8e6422499b3e6dc783

--- dist/release.sh
+++ dist/release.sh
@@ -3,7 +3,15 @@
 # Echo commands as they are executed
 set -x
 
+# Copy over built programs to package directories
 cp ../build-Batlab-Desktop_Qt_5_9_0_GCC_64bit-Release/Batlab linux/packages/com.lexcelon.batlabtoolkitgui/data/Batlab
 
 cp ../build-Batlab-Windows-Release/release/Batlab.exe windows/packages/com.lexcelon.batlabtoolkitgui/data/Batlab.exe
+
+# Clear out repositories and put new ones in their places
+rm -rf ../docs/linux/*
+~/Qt/QtIFW-3.0.1/linux/bin/repogen -p linux/packages/ ../docs/linux/
+
+rm -rf ../docs/windows/*
+wine ~/Qt/QtIFW-3.0.1/windows/bin/repogen.exe -p windows/packages/ ../docs/windows/
 
