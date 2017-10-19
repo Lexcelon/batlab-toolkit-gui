0250f6b3e718d83d4d4bbd92e0fdaa3fdacb247c
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-18 17:52:37 -0700

    add undo steps to linux installer
    Former-commit-id: 1eafc57f19a0ca4af2c8da6b2f6a4fdecbd95b37

--- dist/linux/packages/com.lexcelon.batlabtoolkitgui/meta/installscript.js
+++ dist/linux/packages/com.lexcelon.batlabtoolkitgui/meta/installscript.js
@@ -1,14 +1,14 @@
 function Component() {
     component.addElevatedOperation("Execute", "mkdir", "-p", "@TargetDir@");
     
-    component.addElevatedOperation("Execute", "cp", "/etc/shells", "@TargetDir@/Batlab-run");
+    component.addElevatedOperation("Execute", "cp", "/etc/shells", "@TargetDir@/Batlab-run", "UNDOEXECUTE", "rm", "@TargetDir@/Batlab-run");
     component.addElevatedOperation("Execute", "sed", "-i", 's/.*//', "@TargetDir@/Batlab-run");
     component.addElevatedOperation("Execute", "sed", "-i", '1s/.*/#!\\/bin\\/bash/g', "@TargetDir@/Batlab-run");
     component.addElevatedOperation("Execute", "sed", "-i", '$ acd @TargetDir@', "@TargetDir@/Batlab-run");
     component.addElevatedOperation("Execute", "sed", "-i", '$ a\.\/Batlab\.sh', "@TargetDir@/Batlab-run");
     component.addElevatedOperation("Execute", "chmod", "+x", "@TargetDir@/Batlab-run");
     
-    component.addElevatedOperation("Execute", "ln", "-sf", "@TargetDir@/Batlab-run", "/usr/local/bin/batlab-toolkit-gui");
+    component.addElevatedOperation("Execute", "ln", "-sf", "@TargetDir@/Batlab-run", "/usr/local/bin/batlab-toolkit-gui", "UNDOEXECUTE", "rm", "/usr/local/bin/batlab-toolkit-gui");
     
     
 }
