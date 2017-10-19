749d4995a5a2d236f882de6cbf02914f087f4dbf
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-19 01:06:50 +0000

    change some dir structure for installers
    Former-commit-id: 630f11a0f1b889d649c36c7f867934a9bc109720

--- dist/README.md
+++ dist/README.md
@@ -1,4 +1,3 @@
 1. Build the Windows and Linux programs.
 2. Update the package.xml files.
-
-TODO note that if they do different directory the symlink will not work
+3. 
--- dist/release.sh
+++ dist/release.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-cp ../build-Batlab-Desktop_Qt_5_9_0_GCC_64bit-Release/Batlab linux/packages/com.lexcelon.batlabtoolkitgui.core/data/Batlab
+# Echo commands as they are executed
+set -x
 
-cp ../build-Batlab-Windows-Release/release/Batlab.exe windows/packages/com.lexcelon.batlabtoolkitgui.core/data/Batlab.exe
+cp ../build-Batlab-Desktop_Qt_5_9_0_GCC_64bit-Release/Batlab linux/packages/com.lexcelon.batlabtoolkitgui/data/Batlab
+
+cp ../build-Batlab-Windows-Release/release/Batlab.exe windows/packages/com.lexcelon.batlabtoolkitgui/data/Batlab.exe
 
