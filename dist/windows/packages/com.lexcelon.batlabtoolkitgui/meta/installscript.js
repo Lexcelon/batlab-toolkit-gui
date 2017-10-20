1decafa6c97e5ca0d2a8ed72497df7bf46760f7e
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-19 19:33:21 -0700

    add option in installers to run program after install
    Former-commit-id: 5d2d7c399f7c7068ac2f9580849082e1b5dc4a3d
    Former-commit-id: e712e761dd4e2622c22d43741671e37b70c9d622

--- dist/windows/packages/com.lexcelon.batlabtoolkitgui/meta/installscript.js
+++ dist/windows/packages/com.lexcelon.batlabtoolkitgui/meta/installscript.js
@@ -1,4 +1,11 @@
 function Component()
 {
-    component.addOperation("CreateShortcut", "@TargetDir@/Batlab.exe", "@StartMenuDir@/Batlab Toolkit GUI.lnk", "workingdirectory=@TargetDir@");
+
+}
+
+Component.prototype.createOperations = function()
+{
+    component.createOperations();
+
+    component.addOperation("CreateShortcut", "@TargetDir@/Batlab.exe", "@StartMenuDir@/@Name@.lnk");
 }
