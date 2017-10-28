c5fd2039a51f8b0c57d37a3fd855b7e3b70885ed
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-28 02:04:01 -0600

    try again to fix path for windows compiler
    Former-commit-id: bd1c448d2f7d50c33f1bd0baa78e3bd36f906728

--- build/appveyor/build_windows.cmd
+++ build/appveyor/build_windows.cmd
@@ -6,7 +6,7 @@ echo Setting up environment...
 set PATH=%QT%\bin\;%PATH%
 set PATH=C:\Qt\Tools\QtCreator\bin\;%PATH%
 set PATH=C:\Qt\QtIFW-3.0.1\bin\;%PATH%
-set PATH=C:\Qt\Tools\min530_32\bin\;%PATH%
+set PATH=C:\Qt\Tools\mingw530_32\bin\;%PATH%
 
 echo Building...
 qmake src
