93b291d18ad9602cef743c40e2601740716d8127
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-31 13:33:02 -0500

    copy the executable into the package directory before creating deploy repository
    Former-commit-id: dbd1e07b50b1a6ca181248b744259c6600f2601a

--- build/appveyor/build_windows.cmd
+++ build/appveyor/build_windows.cmd
@@ -8,16 +8,17 @@ set PATH=C:\Qt\Tools\mingw530_32\bin\;%PATH%
 set PATH=C:\MinGW\bin\;%PATH%
 
 echo Setting program version...
-perl.exe -p -i.bak -e "s/0\.0\.0/%APPVEYOR_BUILD_VERSION%/g" src\version.h dist\linux\config\config.xml dist\linux\packages\com.lexcelon.batlabtoolkitgui\meta\package.xml dist\windows\config\config.xml dist\windows\packages\com.lexcelon.batlabtoolkitgui\meta\package.xml
+perl.exe -p -i.bak -e "s/0\.0\.0/%APPVEYOR_BUILD_VERSION%/g" src\version.h dist\windows\config\config.xml dist\windows\packages\com.lexcelon.batlabtoolkitgui\meta\package.xml
 
 echo Setting program date...
-perl.exe -p -i.bak -e "s/2000-01-01/%APPVEYOR_REPO_COMMIT_TIMESTAMP%/g" dist\linux\packages\com.lexcelon.batlabtoolkitgui\meta\package.xml dist\windows\packages\com.lexcelon.batlabtoolkitgui\meta\package.xml
+perl.exe -p -i.bak -e "s/2000-01-01/%APPVEYOR_REPO_COMMIT_TIMESTAMP%/g" dist\windows\packages\com.lexcelon.batlabtoolkitgui\meta\package.xml
 
 echo Building program...
 mkdir temp
 cd temp
 qmake ..\src
 mingw32-make
+xcopy release\Batlab.exe ..\dist\windows\packages\com.lexcelon.batlabtoolkitgui\data\Batlab.exe
 
 echo Building installer...
 binarycreator.exe -p ..\dist\windows\packages\ -c ..\dist\windows\config\config.xml -n Batlab-Toolkit-GUI-Installer-%APPVEYOR_BUILD_VERSION%.exe
