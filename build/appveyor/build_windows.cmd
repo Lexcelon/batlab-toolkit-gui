838ce7551aa94f340d82a9aba65f76a34193de29
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-30 22:45:58 -0500

    try again to fix perl command
    Former-commit-id: 7110fe913bbd099f27caf9b3ebc404f6756430cb

--- build/appveyor/build_windows.cmd
+++ build/appveyor/build_windows.cmd
@@ -10,7 +10,7 @@ set PATH=C:\Qt\Tools\mingw530_32\bin\;%PATH%
 set PATH=C:\MinGW\bin\;%PATH%
 
 echo Setting program version...
-perl.exe -p -i.bak -e 's/0\.0\.0/%APPVEYOR_BUILD_VERSION%/g' src\version.h
+perl.exe -p -i.bak -e "s/0\.0\.0/%APPVEYOR_BUILD_VERSION%/g" src\version.h
 type src\version.h
 
 echo Building program...
