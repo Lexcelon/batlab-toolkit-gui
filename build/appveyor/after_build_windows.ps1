fccf85200114a069245919aafe8e86b9c0c4a2f4
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-31 12:49:10 -0500

    add S3 repo deploy
    Former-commit-id: ce6d799a97bcaa3daeabce7a21e931f0ded53d66

--- build/appveyor/after_build_windows.ps1
+++ build/appveyor/after_build_windows.ps1
@@ -1,6 +1,13 @@
+# Publish installer artifact
+
 Get-ChildItem Batlab-Toolkit-GUI-Installer-*.exe | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
-$root = Resolve-Path .\windows
+# Publish updated binary artifacts
+
+cd windows
+
+$root = Resolve-Path .
 
 [IO.Directory]::GetFiles($root.Path, '*.*', 'AllDirectories') | % { Push-AppveyorArtifact $_ -FileName $_.Substring($root.Path.Length + 1) -DeploymentName to-publish }
 
+cd ..
