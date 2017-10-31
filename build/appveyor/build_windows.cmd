b88a0b8c97ae7bb27abdf29aed90a9af8a3f79d8
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-10-31 11:02:16 -0500

    generate repo with each build
    Former-commit-id: 06d2210faaa19a03ce00290efc34d564e2636c4e

--- build/appveyor/build_windows.cmd
+++ build/appveyor/build_windows.cmd
@@ -1,7 +1,5 @@
 echo on
 
-SET project_dir=%cd%
-
 echo Setting up environment...
 set PATH=C:\Qt\5.9.1\mingw53_32\bin\;%PATH%
 set PATH=C:\Qt\Tools\QtCreator\bin\;%PATH%
@@ -24,3 +22,8 @@ mingw32-make
 echo Building installer...
 binarycreator.exe -p ..\dist\windows\packages\ -c ..\dist\windows\config\config.xml -n Batlab-Toolkit-GUI-Installer-%APPVEYOR_BUILD_VERSION%.exe
 
+echo Creating update repository...
+mkdir windows
+repogen.exe -p ..\dist\windows\packages\ windows\
+
+dir windows\
