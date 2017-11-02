2055e5d1ca680055ac3731fd66a29a02ec2b4a38
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-11-01 19:54:35 -0500

    attempt add windows shortcut on installation
    Former-commit-id: 99e7819373b91c98a1b5c52aaa57a8bbdbe9eb35

--- dist/windows/packages/com.lexcelon.batlabtoolkitgui/meta/installscript.js
+++ dist/windows/packages/com.lexcelon.batlabtoolkitgui/meta/installscript.js
@@ -1,11 +1,42 @@
 function Component()
 {
-
+    console.log("Component");
 }
 
 Component.prototype.createOperations = function()
 {
     component.createOperations();
-
-    component.addOperation("CreateShortcut", "@TargetDir@/Batlab.exe", "@StartMenuDir@/@Name@.lnk");
+    console.log("prototype");
+    console.log("@StartMenuDir@");
+    console.log("@UserStartMenuProgramsPath@");
+    component.addOperation("CreateShortcut", "@TargetDir@/Batlab.exe", "@UserStartMenuProgramsPath@/@StartMenuDir@/Batlab.lnk", "workingDirectory=@TargetDir@");
 }
+
+
+// function Component()
+// {
+// }
+// Component.prototype.isDefault = function()
+// {
+//     return true;
+// }
+// Component.prototype.createOperations = function()
+// {
+//     try
+//     {
+// 	component.createOperations();
+//     }
+//     catch (e)
+//     {
+// 	print(e);
+//     }
+//     if (installer.value("os") === "win")
+//     {
+// 	component.addOperation("CreateShortcut", "@TargetDir@/YourApp.exe", "@DesktopDir@/YourApp_name.lnk");
+//     }
+//     if (installer.value("os") === "x11")
+//     {
+// 	component.addOperation("CreateDesktopEntry", "/usr/share/applications/YourApp.desktop", "Version=1.0\nType=Application\nTerminal=false\nExec=@TargetDir@/YourApp.sh\nName=YourApp_name\nIcon=@TargetDir@YourApp_icon.png\nName[en_US]=YourApp_name");
+// 	component.addElevatedOperation("Copy", "/usr/share/applications/YourApp.desktop", "@HomeDir@/Desktop/YourApp.desktop");
+//     }
+// }
